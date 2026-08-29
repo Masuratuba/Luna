19b003ed8f5a8c82d0db3fc26d23159653069b36
Luna <masuramogs@icloud.com>
2026-08-29 23:43:38 +0200

    feat: add Luna version manager

---
+++ lib/luna/version.ts
@@ -0,0 +1,3 @@
+export const LUNA_VERSION = "0.2.0-foundation";
+export type VersionInfo = { version: string; schemaVersion: number; rulesVersion: number };
+export const VERSION_INFO: VersionInfo = { version: LUNA_VERSION, schemaVersion: 1, rulesVersion: 100 };
