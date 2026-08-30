b0f8cf39324405f39045875263823a769ce8a2f2
Luna <masuramogs@icloud.com>
2026-08-30 07:55:43 +0200

    Use FlatCompat for Next ESLint legacy config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,17 @@
+import { dirname } from "node:path";
+import { fileURLToPath } from "node:url";
+import { FlatCompat } from "@eslint/eslintrc";
 import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
 
 export default defineConfig([
-  nextVitals,
+  ...compat.extends("next/core-web-vitals"),
   globalIgnores([
     ".next/**",
     "out/**",
