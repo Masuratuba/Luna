58cdd2b6e577a7a76d045f1a45135e95fdab2eb1
Luna <masuramogs@icloud.com>
2026-09-04 11:38:29 +0200

    Checkpoint 63: use project test runner

--- lib/providers/mail-send-provider.test.ts
+++ lib/providers/mail-send-provider.test.ts
@@ -1,4 +1,4 @@
-import { describe, expect, it } from "vitest";
+import { describe, expect, it } from "node:test";
 import { DurableMailSendProvider } from "./mail-send-provider";
 
 describe("DurableMailSendProvider", () => {
