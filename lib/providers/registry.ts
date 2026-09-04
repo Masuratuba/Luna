2df98a65ca660f37b1548fa03f59e2e711270cae
Luna <masuramogs@icloud.com>
2026-09-04 13:21:50 +0200

    Checkpoint 65: register mail send and audit providers

--- lib/providers/registry.ts
+++ lib/providers/registry.ts
@@ -1,8 +1,10 @@
 import { DisabledFinancialProvider } from "./financial-provider";
 import { HttpAnalyticsProvider, HttpCommerceProvider, HttpSearchProvider } from "./http-providers";
 import { DurableMailProvider } from "./mail-provider";
+import { DurableAuditProvider } from "./audit-provider";
 import { HttpWebFetchProvider } from "./web-provider";
 import { OpenAIProvider } from "./openai-provider";
+import { DurableMailSendProvider } from "./mail-send-provider";
 
 export function createProviderRegistry() {
   return {
@@ -10,6 +12,8 @@ export function createProviderRegistry() {
     search: () => new HttpSearchProvider(),
     web: () => new HttpWebFetchProvider(),
     mail: () => new DurableMailProvider(),
+    mailSend: () => new DurableMailSendProvider(),
+    audit: () => new DurableAuditProvider(),
     analytics: () => new HttpAnalyticsProvider(),
     commerce: () => new HttpCommerceProvider(),
     financial: () => new DisabledFinancialProvider(),
